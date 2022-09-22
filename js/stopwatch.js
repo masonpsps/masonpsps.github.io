cfe03b57924004170f4bb8f79a259fcb8c844a8f
masonpsps <masonpsipes@gmail.com>
2022-09-21 22:44:39 -0500

    fixed lap functionality and display

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -7,13 +7,19 @@ const lapBtn = document.querySelector('.lap-btn');
 const sideNav = document.querySelector('.sidenav');
 const dropdown = document.querySelector('.dropdown');
 const dropdownList = document.querySelector('.dropdown-list');
-const lapRecord = document.querySelector('#lap-record');
+const lapRecord = document.querySelector('.lap-record');
+// const $ = window.jQuery;
 let startTime = 0;
 let t;
 let state = 0;
 let elapsedTime = 0;
 let date = 0;
 let lapNow = null;
+let lastLap = 0;
+let lapTime = { min: 0, sec: 0, msec: 0};
+let lapNumber = 1;
+let currentLapTime = document.querySelector('.current-lap p.lap-time');
+let diff = 0, prevDiff = 0;
 
 // TODO: add lap button functionality
 
@@ -38,32 +44,118 @@ function resetTimer() {
     seconds.innerHTML = "00";
     minutes.innerHTML = "00";
     elapsedTime = 0;
+    lastLap = 0;
     state = 0;
+    lapNumber = 1;
+    lapTime = {
+        min: 0,
+        sec: 0,
+        msec: 0
+    };
 
     startBtn.innerHTML = 'Start';
+
+    // lapRecord.innerHTML = `
+    //     <li class="current-lap">
+    //         <p>Lap <span class="lap-number">1</span></p>
+    //         <p class="lap-time" id="lap-1">
+    //             <span class="lap-min">00</span>:
+    //             <span class="lap-sec">00</span>.
+    //             <span class="lap-msec">00</span>
+    //         </p>                     
+    //     </li>`
+    // ;
+    lapRecord.innerHTML = '';
+    addLap(lapNumber, lapTime.min, lapTime.sec, lapTime.msec);
 }
 
 function updateTime() {
     date = Date.now();
     elapsedTime = Date.now() - startTime;
-    let ms = elapsedTime % 1000;
-    let s = Math.floor(elapsedTime / 1000) % 60;
-    let m = Math.floor(elapsedTime / 60000) % 60;
 
-    mseconds.innerHTML = ms.toString().padStart(3, '0').substring(0, 2);
-    seconds.innerHTML = s.toString().padStart(2, '0');
-    minutes.innerHTML = m.toString().padStart(2, '0');
+    minutes.innerHTML = parseTime(elapsedTime, 'min');
+    seconds.innerHTML = parseTime(elapsedTime, 'sec');
+    mseconds.innerHTML = parseTime(elapsedTime, 'msec');
+
+    lapTime = { 
+        min: parseTime(elapsedTime - lastLap, 'min'), 
+        sec: parseTime(elapsedTime - lastLap, 'sec'), 
+        msec: parseTime(elapsedTime - lastLap, 'msec')
+    };
+    updateCurrentLap();
+}
+
+function parseTime(time, unit) {
+    let parsedTimeUnit = 0;
+    switch(unit) {
+        case 'min':
+            parsedTimeUnit = Math.floor(time / 60000) % 60;
+            parsedTimeUnit = parsedTimeUnit.toString().padStart(2, '0');
+            break;
+        case 'sec':
+            parsedTimeUnit = Math.floor(time / 1000) % 60;
+            parsedTimeUnit = parsedTimeUnit.toString().padStart(2, '0');
+            break;
+        case 'msec':
+            parsedTimeUnit = time % 1000;
+            parsedTimeUnit = parsedTimeUnit.toString().padStart(3, '0').substring(0, 2);
+            break;
+    }
+    
+    return parsedTimeUnit;
+}
+
+function updateCurrentLap() {
+    let currLapNow = `
+        <span class="lap-min">${lapTime.min}</span>:
+        <span class="lap-sec">${lapTime.sec}</span>.
+        <span class="lap-msec">${lapTime.msec}</span>
+    `;
+    currentLapTime.innerHTML = currLapNow;
 }
 
 function lapTimer() {
-    lapNow = `
-        <li class="lap">
-            ${minutes} : 
-            ${seconds} . 
-            ${mseconds}
-        </li>
+    diff = elapsedTime - lastLap;
+
+    let str = '';
+    if (diff < prevDiff + 10 && diff > prevDiff - 10) {
+        // do nothing
+    } else if(diff > prevDiff) {
+        str = 'slower-anim';
+    } else if(diff < prevDiff) {
+        str = 'faster-anim';
+    }
+    if(str) { 
+        let id = currentLapTime.id;
+        $(`#${id}`)?.addClass(str)
+        .delay($(`#${id}`)?.eq(0)?.css('animation-duration')?.slice(0, -1) * 1000 || 1500)
+        .queue( 
+            function() {
+                $(`#${id}`).removeClass(str);
+            }
+        );
+    }
+    prevDiff = diff;
+    lastLap = elapsedTime;
+
+    currentLapTime.parentElement.classList.remove('current-lap');
+
+    lapNumber++;
+    addLap(lapNumber, lapTime.min, lapTime.sec, lapTime.msec);
+}
+function addLap(lapNum, lapMin, lapSec, lapMsec) {
+    let listItem = document.createElement('li');
+    listItem.classList.add('current-lap');
+    listItem.innerHTML = `
+        <p>Lap <span class="lap-number">${lapNum}</span></p>
+        <p class="lap-time" id="lap-${lapNum}">
+            <span class="lap-min">${lapMin.toString().padStart(2, '0')}</span>:
+            <span class="lap-sec">${lapSec.toString().padStart(2, '0')}</span>.
+            <span class="lap-msec">${lapMsec.toString().padStart(2, '0')}</span>
+        </p>
     `;
-    lapRecord.innerHTML += lapNow;
+    lapRecord.appendChild(listItem);
+    currentLapTime = document.querySelector('.current-lap p.lap-time');
 }
 
 function openNav() {
@@ -80,7 +172,23 @@ function dropdownMenu() {
     dropdownList.classList.toggle('border');
 }
 
-startBtn.addEventListener('click', function() {     // probably should be by classes
+window.onload = () => {
+    // lapRecord.innerHTML = `                    
+    //     <li class="current-lap">
+    //         <p>Lap <span class="lap-number">1</span></p>
+    //         <p class="lap-time" id="lap-1">
+    //             <span class="lap-min">00</span>:
+    //             <span class="lap-sec">00</span>.
+    //             <span class="lap-msec">00</span>
+    //         </p>                            
+    //     </li>
+    // `;
+    addLap(1, 0, 0, 0);
+
+    currentLapTime = document.querySelector('.current-lap p.lap-time');
+}
+
+startBtn.addEventListener('click', function() {
     (state === 0 || state === 2)
         ? startTimer()
         : pauseTimer();
