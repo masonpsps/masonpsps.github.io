25ad0c61d640563f143a0c1560e7c9216da40ed8
masonpsps <masonpsipes@gmail.com>
2022-11-27 21:06:52 -0600

    show popup when clicked

--- js/recipes.js
+++ js/recipes.js
@@ -41,22 +41,21 @@ function findAllMeals() {
     Promise.all(fetches).then(() => {
         pulledMeals.push(result.flat());
         console.log(pulledMeals.flat());
-        displayMealSection(categorySection, [1, 2, 3]);
+        displayMealSection(categorySection);
     });        
 }
 
-function displayMealSection(sectionToDisplay, indicesToShow) {
+function displayMealSection(sectionToDisplay, indicesToShow = randomIndices(4)) {
     let str = '';
     for(let i = 0; i < indicesToShow.length; i++) {
-        let random = Math.floor(Math.random() * pulledMeals[0].length);
         str += `
-            <div class="search-result" onclick="displayPopupByID(${pulledMeals[0][random].idMeal})">
+            <div class="search-result" onclick="displayPopup(${indicesToShow[i]})">
                 <div class="result-img">
-                    <img src="${pulledMeals[0][random].strMealThumb}" alt="">
+                    <img src="${pulledMeals[0][indicesToShow[i]].strMealThumb}" alt="">
                 </div>
                 <div class="result-banner">
-                    <span class="result-name">${pulledMeals[0][random].strMeal}</span>
-                    <span class="result-fav" onclick="addMealToSavedByID(${pulledMeals[0][random].idMeal})">
+                    <span class="result-name">${pulledMeals[0][indicesToShow[i]].strMeal}</span>
+                    <span class="result-fav" onclick="addMealToSavedByID(${pulledMeals[0][indicesToShow[i]].idMeal})">
                     <span onclick="changeHeartOnClick(this)">
                         <i class="far fa-heart"></i>
                     </span>
@@ -67,6 +66,115 @@ function displayMealSection(sectionToDisplay, indicesToShow) {
     }
     sectionToDisplay.innerHTML += str;
 }
+function randomIndices(howMany = 3, max = pulledMeals[0].length) {
+    const arr = [];
+    for(let i = 0; i < howMany; i++) {
+        arr.push(Math.floor(Math.random() * max));
+    }
+
+    return arr;
+}
+
+function displayPopup(index) {
+    if(!index) {
+        popupDisplay.parentElement.parentElement.classList.add('hidden');
+        // document.documentElement.style.overflow = 'scroll';
+        // document.body.scroll = 'no';
+        return;
+    }
+    
+    popupDisplay.parentElement.parentElement.classList.remove('hidden');
+    popupDisplay.scrollTo(0, 0);
+    // document.documentElement.style.overflow = 'hidden';
+    // document.body.scroll = 'yes';
+
+    const meal = pulledMeals[0][index];
+    const measuredIngredients = getIngredientsWithMeasurements(meal);
+    const formattedDirections = formatDirections(meal);
+
+    popupDisplay.innerHTML = `
+        <div class="close-popup" onclick="displayPopupByIndex(false)"><i class="fa fa-close"></i></div>
+        <div class="img-title-wrapper">
+            <div class="popup-img"><img src="${meal.strMealThumb}" alt="img"></div>
+            <div class="popup-title">${meal.strMeal}</div>
+        </div>
+        <div class="ingredients">
+            <span class="section-title">Ingredients</span>
+            <ul class="ingredient-list">
+                ${measuredIngredients}
+            </ul>
+        </div>
+        <div class="directions">
+            <span class="section-title">Directions</span>
+            <p class="full-directions">${formattedDirections}</p>
+        </div>
+    `;
+}
+function getIngredientsWithMeasurements(meal) {
+    /*
+    This function formats the ingredients from the given meal
+    and fills a list containing the measurement and corresponding
+    ingredient
+
+    API lists ingredients as strIngredient# and measurements as strMeasurement#
+    where # is from 1 to 20
+    */
+    let measurements = [];
+    let ingredients = [];
+    let ingrListString = '';
+
+    for(let i = 1; i < 21; i++) {
+        if(meal['strIngredient'.concat(i)]) {
+            ingredients[i - 1] = meal['strIngredient'.concat(i)];
+            measurements[i - 1] = meal['strMeasure'.concat(i)];
+        } else {
+            break;
+        }
+    }
+    for(let i = 0; i < measurements.length; i++) {
+        ingrListString += `
+            <li>
+                <span class="measurement">${measurements[i]} </span>
+                <span class="ingredient">${ingredients[i]}</span>
+            </li>
+        `;
+    }
+
+    return ingrListString;
+}
+function formatDirections(meal) {
+    /*
+    This function formats the directions from the given meal and
+    returns a string with a label for each step followed by the 
+    instruction for that step
+
+    API stores directions as a single string with new steps generally
+    designated by \r\n so regex is used to split the string into an array
+    relatively inconsistent however(many directions already contain step 
+    labels as STEP X or X.) so regex implemented to remove array elements
+    or substrings matching those formats
+    */
+    let unformatted = meal.strInstructions;
+    let n = unformatted.split(/\r\n/g);
+    let formattedDir = '';
+    let stepNum = 1;
+    for(let i = 0; i < n.length; i++) {
+        if(n[i] !== "" && n[i] !== " ") {
+            if(/^(\s*\d+.\s*)/i.test(n[i])) {
+                n[i] = n[i].replace(/^(\s*STEP\s*\d*|\s*\d+.\s*)/i, '');
+            } else if(/^(\s*STEP\s*\d*)/i.test(n[i])) {
+                n[i] = '';
+            }
+            if(n[i] !== '' && n[i] !== " ") {
+                formattedDir += `
+                    <p class="direction-step">Step ${stepNum}</p>
+                    <p class="">${n[i]}</p>
+                `;
+                stepNum++; 
+            }
+        }
+    }    
+    return formattedDir;
+}
 
 findAllMeals();
-// displayMealSection(categorySection, [1, 2, 3]);
